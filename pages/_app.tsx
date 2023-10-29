1593a853de33baaab988df06200057ccf8d6303d
Gustavo Lucena <guspibe15@gamil.com>
2023-10-29 19:14:05 -0400

    add(package) adding atomic state for global state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
+import { AtomicState } from "atomic-state"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <AtomicState>
+        <Component {...pageProps} />
+      </AtomicState>
     </ThemeProvider>
   )
 }
