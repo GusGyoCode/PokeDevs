f2f115d4c9fe742af67a6be36218eb5b81a36dfd
Gustavo Lucena <guspibe15@gamil.com>
2023-10-29 12:11:01 -0400

    add(file and enpoint) endpoint for login and file JSON with users for filter user

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import path from "path"
+import { promises as fs } from "fs"
+
+interface Data {
+  user?: any
+  error?: string
+}
+
+export default async function handlerLogin(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { email, password } = req.body
+  if (req.method === "POST") {
+    if (email !== "" && password !== "") {
+      const jsonDirectory = path.join(process.cwd(), "json")
+      const fileContents = await fs.readFile(
+        jsonDirectory + "/users.json",
+        "utf8",
+      )
+      const user = JSON.parse(fileContents).data.filter(
+        (e: any) => e.email === email,
+      )
+      if (user.length > 0) {
+        if (user[0].password === password) {
+          const newUser = {
+            name: user[0].name,
+            lastName: user[0].lastName,
+            email: user[0].email,
+            token: "hola",
+          }
+          res.status(200).json({ user: newUser })
+        } else {
+          res.status(403).json({ error: "Contraseña invalida" })
+        }
+      } else {
+        res.status(400).json({ error: "Usuario no encontrado" })
+      }
+    } else {
+      res
+        .status(400)
+        .json({ error: "No hay datos para Usuario o para Password" })
+    }
+  } else {
+    res.status(500).json({
+      error: `El methodo ${req.method} no coincide con el methodo de la ruta`,
+    })
+  }
+}
