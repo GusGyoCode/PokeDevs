b2f67485098098b856190cc130b48f15f0377047
Gustavo Lucena <guspibe15@gamil.com>
2023-10-29 23:14:12 -0400

    Enabling CORS for all API routes

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -12,6 +12,18 @@ export default async function handlerLogin(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  res.setHeader("Access-Control-Allow-Credentials", "true")
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  )
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  )
   const { email, password } = req.body
   if (req.method === "POST") {
     if (email !== "" && password !== "") {
