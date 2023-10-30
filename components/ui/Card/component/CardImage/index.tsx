e071b82f8cf134c001a253796ff42f791359b718
Gustavo Lucena <guspibe15@gamil.com>
2023-10-30 10:47:08 -0400

    feat(error images cards)

--- components/ui/Card/component/CardImage/index.tsx
+++ components/ui/Card/component/CardImage/index.tsx
@@ -1,19 +1,44 @@
 import Image from "next/image"
 import useFetch from "http-react"
+import Button from "@/components/ui/Button"
 
 export default function CardImage({ url }: { url: string }) {
-  const { data, loading } = useFetch(url, {
+  const { data, loading, error, reFetch } = useFetch(url, {
     onResolve(data) {
       console.log(data)
     },
   })
   return (
-    <Image
-      src={loading ? "/img/loadingImage.png" : data.sprites.front_default}
-      alt="pokemon"
-      width={320}
-      height={270}
-      className={`w-32 h-32 mx-auto ${loading && "animate-pulse"}`}
-    />
+    <div className="flex items-center flex-col gap-2">
+      <Image
+        src={
+          loading
+            ? "/img/loadingImage.png"
+            : // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+            error
+            ? "/img/errorImage.png"
+            : // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+            !data.sprites.front_default
+            ? "/img/loadingImage.png"
+            : data.sprites.front_default
+        }
+        alt="pokemon"
+        width={320}
+        height={270}
+        className={`w-32 h-32 mx-auto ${loading && "animate-pulse"}`}
+      />
+      {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        error && (
+          <>
+            <h2 className="text-center text-xs text-red-500">
+              Error al cargar la imagen intente de nuevo
+            </h2>
+            {/* eslint-disable-next-line @typescript-eslint/promise-function-async */}
+            <Button click={() => reFetch()}>Recargar</Button>
+          </>
+        )
+      }
+    </div>
   )
 }
